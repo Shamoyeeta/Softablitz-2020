09c45189c84268d6b4b4d545825114b017f92da0
shamoyeeta <sahashamoyeeta@yahoo.com>
2020-10-10 13:54:36 +0530

    Added User class and Product class

---
+++ src/Product.java
@@ -0,0 +1,38 @@
+class Product {
+    double price, quantity,threshold_quantity;
+    String name, category;
+    String productCode;
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+}
